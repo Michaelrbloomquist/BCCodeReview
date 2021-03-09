f4afed0ce61eb7a6fe0c95182ec0f119cd2f2bff
Michael Bloomquist <michaelrbloomquist@gmail.com>
2021-03-09 15:56:16 -0800

    Revert "Update RevDis.js"
    This reverts commit a1e5358ab9647bcd0f16ebb79698c7a9c66711de.

--- React/src/RevDis.js
+++ React/src/RevDis.js
@@ -22,22 +22,10 @@ class RevDis extends React.Component {
             fname: '',
             step: -1,
             CookieSave: '',
-            cHld: [],
-            isOpen: false,
             fileContent: ''
+
         };
         this.handleiUserNChange = this.handleiUserNChange.bind(this);
-    }
-
-    openPopup = () => {
-        this.setState({
-            isOpen: true
-        });
-    }
-    closePopup = () => {
-        this.setState({
-            isOpen: false
-        });
     }
 
     handleiUserNChange(evt) {
@@ -68,20 +56,6 @@ class RevDis extends React.Component {
                 })
             })
         }
-    }
-
-    loadCollab = async () => {
-        await axios.get("http://localhost:3002/WORKS_ON_REVIEWS/", {
-            headers: {accesstoken: this.state.CookieSave, test: this.state.routePara}
-        }).then(res => {
-            var tHld = []
-            for(var i = 0; i < res.data.length; i++){
-                tHld[i] = res.data[i].UNameW
-            }
-            this.setState({
-                cHld: tHld
-            })
-        })
     }
 
     invitingUser = async () => {
@@ -140,19 +114,6 @@ class RevDis extends React.Component {
         this.loadCollab();
     }
 
-    confirmDel = async () => {
-        await axios.delete("http://localhost:3002/WORKS_ON_REVIEWS/" + this.state.routePara, {
-            headers: {accesstoken: this.state.CookieSave}
-        })
-        await axios.delete("http://localhost:3002/INVITES/" + this.state.routePara, {
-            headers: {accesstoken: this.state.CookieSave}
-        })
-        await axios.delete("http://localhost:3002/REVIEW/" + this.state.routePara, {
-            headers: {accesstoken: this.state.CookieSave}
-        })
-        return window.location = "/ProjectsTest"
-    }
-
     updatingReview = async () => {
         this.setState({
             step: 3
@@ -184,12 +145,6 @@ class RevDis extends React.Component {
 
     render() {
 
-        let popup = null;
-        if(this.state.isOpen){
-            popup = (<Popup  message={<div><p>This is permanent, and cannot be reversed</p><input type='submit' className='submit' value='Are you sure?' onClick={this.confirmDel}/></div>} closeMe={this.closePopup}/>);
-        }
-
-        const items = this.state.cHld.map((item, i) =><div key = {i}>{item}</div>)
 
         switch (this.state.authState) {
             case ('loading'):
@@ -199,9 +154,6 @@ class RevDis extends React.Component {
                     <div className='grad1'>
                         <NavBar/>
                         <br></br>
-                        <div className = 'inline'>
-                            <div>Collaborators: {items}</div>
-                        </div>
                         <div className='container'>
                             <input type="submit" className='submit' value="Invite a User to Review"
                                    onClick={this.invitingUser}/>
@@ -233,8 +185,6 @@ class RevDis extends React.Component {
                             <p style={{whiteSpace: 'pre'}}>{this.state.gotRev}</p>
                         </div>
                         <br></br>
-                        <input type='submit' className='submit' value="Delete Review" onClick={this.openPopup}/>
-                        {popup}
                     </div>
                 );
             case ('unauthorized'):
